751866c82a93509d4194522431d4ced9da0d6f50
regisattler <reginaldo.sattler@gmail.com>
2020-02-25 17:46:11 -0300

    add config cors

--- dist/startUp.js
+++ dist/startUp.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const dataBase_1 = require("./infra/dataBase");
 const newsController_1 = require("./controller/newsController");
 class StartUp {
@@ -12,7 +13,15 @@ class StartUp {
         this.middler();
         this.routes();
     }
+    enableCors() {
+        const options = {
+            methods: "GET, OPTIONS, PUT,POST,DELETE",
+            origin: "*"
+        };
+        this.app.use(cors(options));
+    }
     middler() {
+        this.enableCors();
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: false }));
     }
